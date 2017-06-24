e359050582dca6bb8bf3f64ec66e2123adbe3fbe
yarwest <yarnoboelens@gmail.com>
2017-06-24 02:10:47 +0200

    Converted all files to ogg and made the functionality a little bit nicer.

--- soundboard.py
+++ soundboard.py
@@ -1,13 +1,14 @@
 import pygame
 
 pygame.init()
+pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
 screen = pygame.display.set_mode((750,750)#, pygame.FULLSCREEN, 16
     )
 color = (0, 128, 255)
 font=pygame.font.Font(None,30)
 done = False
-buttons = [[30,30,"Pak ze", "pak-ze.mp3"],[30,100,"Pang boem", "pang-boem.mp3"],[30,170,"Margriet", "margriet.mp3"],[30,240,"Badkamergesprekken", "badkamergesprekken.mp3"]]
-buttonWidth = 180
+buttons = [[30,30,"Pak ze", "pak-ze"],[30,100,"Pang boem", "pang-boem"],[30,170,"Margriet", "margriet"],[30,240,"Badkamergesprekken", "badkamergesprekken"]]
+buttonWidth = 240
 buttonHeight = 60
 
 sounds = []
@@ -17,7 +18,7 @@ def initButtons():
         pygame.draw.rect(screen, color, pygame.Rect(button[0], button[1], buttonWidth, buttonHeight))
         buttonText=font.render(button[2], 1,(0,0,0))
         screen.blit(buttonText, (button[0]+15, button[1]+20))
-        sounds.append(pygame.mixer.Sound('audio/'+button[3]))
+        sounds.append(pygame.mixer.Sound('audio/'+button[3]+'.ogg'))
 
 initButtons()
 
@@ -28,5 +29,6 @@ while not done:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             for index, button in enumerate(buttons):
                 if event.pos[0] >= button[0] and event.pos[0] <= button[0]+buttonWidth and event.pos[1] >= button[1] and event.pos[1] <= button[1]+buttonHeight:
-                    sounds[index].play()
+                    pygame.mixer.stop()
+                    sounds[index].play(0)
         pygame.display.flip()
