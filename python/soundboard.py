9b495d406efea3d84d38a543cf7316c4fd92f6b9
yarwest <yarnoboelens@gmail.com>
2017-06-24 23:21:17 +0200

    Altered the file structure to allow multiple (crossplatform) variations of the soundboard app. Also adapted the python script to offer a cross-os solution for reading the files from the audio folder.

--- python/soundboard.py
+++ python/soundboard.py
@@ -1,4 +1,5 @@
 import pygame
+from os.path import dirname, abspath
 
 pygame.init()
 pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
@@ -7,7 +8,13 @@ screen = pygame.display.set_mode((750,750)#, pygame.FULLSCREEN, 16
 color = (0, 128, 255)
 font=pygame.font.Font(None,30)
 done = False
-buttons = [[30,30,"Twee men", "twee-men"],[30,100,"Pak ze", "pak-ze"],[30,170,"Zo jammer", "zo-jammer"]#,[30,170,"Pang boem", "pang-boem"],[30,240,"Margriet", "margriet"],[30,310,"Badkamergesprekken", "badkamergesprekken"]
+buttons = [
+        [30,30,"Twee men", "twee-men"],
+        [30,100,"Pak ze", "pak-ze"],
+        [30,170,"Zo jammer", "zo-jammer"]#,
+        #[30,170,"Pang boem", "pang-boem"],
+        #[30,240,"Margriet", "margriet"],
+        #[30,310,"Badkamergesprekken", "badkamergesprekken"]
     ]
 buttonWidth = 240
 buttonHeight = 60
@@ -19,7 +26,7 @@ def initButtons():
         pygame.draw.rect(screen, color, pygame.Rect(button[0], button[1], buttonWidth, buttonHeight))
         buttonText=font.render(button[2], 1,(0,0,0))
         screen.blit(buttonText, (button[0]+15, button[1]+20))
-        sounds.append(pygame.mixer.Sound('audio/'+button[3]+'.ogg'))
+        sounds.append(pygame.mixer.Sound(dirname(dirname(abspath(__file__)))+'/audio/'+button[3]+'.ogg'))
 
 initButtons()
 
@@ -31,5 +38,5 @@ while not done:
             for index, button in enumerate(buttons):
                 if event.pos[0] >= button[0] and event.pos[0] <= button[0]+buttonWidth and event.pos[1] >= button[1] and event.pos[1] <= button[1]+buttonHeight:
                     pygame.mixer.stop()
-                    sounds[index].play(0)
+                    sounds[index].play()
         pygame.display.flip()
