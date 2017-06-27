873aa3e6cccb6873ced364ec08fba7ef4e9d1d60
yarwest <yarnoboelens@gmail.com>
2017-06-27 19:55:48 +0200

    [WEB] The javascript now automatically adds all the items to the html page. All info is hardcoded though.

--- web/index.js
+++ web/index.js
@@ -3,7 +3,74 @@
 	Soundboard index.js
 	created on: 25-06-2017
 */
+
+var files = [
+    {
+        name:'pak-ze',
+        title:'Pak ze',
+        author:'flokkie'
+    },
+    {
+        name:'koekoek',
+        title:'Koekoek',
+        author:'flokkie'
+    },
+    {
+        name:'twee-men',
+        title:'Twee men',
+        author:'flokkie'
+    },
+    {
+        name:'verkeerde-knopje',
+        title:'Verkeerde knopje',
+        author:'flokkie'
+    },
+    {
+        name:'koekoek',
+        title:'Koekoek',
+        author:'flokkie'
+    },
+    {
+        name:'wtf-ricardo',
+        title:'WTF Ricardo?!',
+        author:'flokkie'
+    },
+    {
+        name:'jaja',
+        title:'Jaja ja',
+        author:'flokkie'
+    },
+    {
+        name:'hey-klootzak',
+        title:'Hey! Klootzak!',
+        author:'jos'
+    },
+    {
+        name:'kutkutkut',
+        title:'Kutkutkut',
+        author:'jelle'
+    },
+    {
+        name:'zo-jammer',
+        title:'Zo jammer',
+        author:'yarno'
+    }
+];
+
 var audio = new Audio();
+
+files.forEach(function(file){
+    var p = document.createElement("p");
+    var text = document.createTextNode(file.title);
+    p.appendChild(text);
+    var div = document.createElement("div");
+    div.appendChild(p);
+    div.className += "soundItem " + file.author;
+    div.onclick = function(){
+        play(file.name);
+    };
+    document.getElementById('page-body').appendChild(div);
+});
 
 function play(file) {
     audio.pause()
