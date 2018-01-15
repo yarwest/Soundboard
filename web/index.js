e80c2d33ac5d18b4d09cb1bf37405eb514fd6077
yarwest <yarnoboelens@gmail.com>
2018-01-16 00:12:41 +0100

    [WEB] Refactored existing JS to more easily redraw sound items. Integrated control option container for future usage.

--- web/index.js
+++ web/index.js
@@ -5,23 +5,31 @@
  */
 
 var audio = new Audio(),
-    pageBody = document.getElementById('page-body');
+    container = document.getElementById('soundItemContainer');
 
-files.forEach(function(file){
-    var p = document.createElement("p"),
-        text = document.createTextNode(file.title);
-    p.appendChild(text);
-    var div = document.createElement("div");
-    div.appendChild(p);
-    div.className += "soundItem " + file.author;
-    div.onclick = function(){
-        play(file.name);
-    };
-    pageBody.appendChild(div);
-});
+draw();
+
+function draw() {
+    files.forEach(function(file){
+        var p = document.createElement("p"),
+            text = document.createTextNode(file.title);
+        p.appendChild(text);
+        var div = document.createElement("div");
+        div.appendChild(p);
+        div.className += "soundItem " + file.author;
+        div.onclick = function(){
+            play(file.name);
+        };
+        container.appendChild(div);
+    });
+}
 
 function play(file) {
     audio.pause()
     audio = new Audio('../audio/mp3/'+file+'.mp3');
     audio.play();
+}
+
+function sortByAuthor() {
+    alert("sorted");
 }
